cd17bf313fe30a31b0a7b8521820626c2402bc21
Kris De Volder <kris.de.volder@gmail.com>
2012-01-26 11:07:26 -0800

    Remove accidentally added character in front of file

--- bundles/org.eclipse.orion.client.core/web/orion/widgets/OpenResourceDialog.js
+++ bundles/org.eclipse.orion.client.core/web/orion/widgets/OpenResourceDialog.js
@@ -1,4 +1,4 @@
-v/*******************************************************************************
+/*******************************************************************************
  * @license
  * Copyright (c) 2010, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made 
