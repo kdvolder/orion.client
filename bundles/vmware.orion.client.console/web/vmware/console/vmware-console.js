ad86988b9e30104f5f1dd62786da3fed31f5feb7
Kris De Volder <kris.de.volder@gmail.com>
2012-03-05 16:38:03 -0800

    gcli orion console

---
+++ bundles/vmware.orion.client.console/web/vmware/console/vmware-console.js
@@ -0,0 +1,290 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     Kris De Volder (VMWare) - initial API and implementation
+ *
+ *******************************************************************************/ 
+ /*global define require*/
+ define(['dojo'], function () { 
+	//We are being passed in a 'fake' gcli that registers commands with orion
+	//service instead of the real gcli.
+ 
+	var dojo = require('dojo');
+ 
+	/////// implementation of 'vmc exec functions commands ////////////////////////////////
+	
+	function defaultCommandExec(commandPath) {
+		return function (args, context) {
+			var promise = new dojo.Deferred();
+			var location = context.location;
+			var wsLoc = context.workspaceLocation;
+			if (typeof(wsLoc)!=='string') {
+				return 'ERROR: could not determine workspace location';
+			}
+			if (typeof(location)!=='string') {
+				return 'ERROR: could not determine working directory location';
+			}
+			dojo.xhrGet({
+				url: '/shellapi/'+commandPath, 
+				headers: {
+					"Orion-Version": "1"
+				},
+				content: { 
+					"location":  location,
+					"user.home": wsLoc,
+					"arguments": JSON.stringify(args) 
+				},
+				handleAs: "text",
+//				timeout: 15000,
+				load: function (result) {
+					promise.callback(result);
+				},
+				error: function(error, ioArgs) {
+					promise.callback(error.message || 'ERROR');
+				}
+			});
+			return promise;
+		};
+	}
+	
+	function vmcCommandExec(commandName) {
+		return defaultCommandExec('vmc/'+commandName);
+	}
+	
+	var execVmcApps = vmcCommandExec('apps');
+	var execVmcGetTarget = vmcCommandExec('get-target');
+	var execVmcLogin = vmcCommandExec('login');
+	var execVmcSetTarget = vmcCommandExec('set-target');
+	var execVmcPush = vmcCommandExec('push');
+	
+	/////// implementation of 'npm install' command ////////////////////////////////////////
+	
+	//TODO: node.js commands should not be in this module, they should be in a plugin or something.
+	//  this will require making it possible for plugins to contribute commands to gcli UI.
+	
+	function initNpmCommands(gcli) {
+		gcli.addCommand({
+			name: 'npm',
+			description: 'Node package manager'
+		});
+
+		gcli.addCommand({
+			name: 'npm install',
+			description: 'install node packages',
+			manual: 'This command installs node packages and any packages that they depend on. It resolves dependencies by talking to the npm registry. ' +
+				'If no packages are specified, then packages to install are determined from the "package.json" file if it can be found.',
+			exec: defaultCommandExec('npm/install'),
+			params: [
+			    {
+					name: 'packages',
+					type: { name: 'array', subtype:'string'},
+					defaultValue: null,
+					description: 'package',
+					manual: 
+						'A package to install. Can be given in one of the following formats: \n'+
+						'<tarball file>\n' +
+						'<tarball url>\n' +
+						'<name>@<tag>\n' +
+						'<name>@<version>\n' +
+						'<name>@<version_range>'
+			    },
+			    {
+					group: 'Options',
+					params: [
+						{
+							name: 'force', 
+							type: 'boolean',
+							description: 'force',
+							manual: 'Force npm to fecth remote resources even ' +
+								'if a local copy exists on disk'
+		//					defaultValue: false
+						}
+					]
+			    }
+			]
+		});
+	}
+	
+	function simpleVMCCommand(gcli, name) {
+		gcli.addCommand({
+			name: 'vmc '+name,
+			description: name+' a cloudfoundry app',
+			manual: 'A nice manual for vmc '+name+' goes here',
+			params: [
+				{
+					name: 'app-name',
+					type: 'string',
+					description: 'Name of app'
+				}
+			],
+			exec: vmcCommandExec(name)
+		});
+	}
+
+	function simpleVMCCommands(gcli, names) {
+		for (var i = 0; i < names.length; i++) {
+			simpleVMCCommand(gcli, names[i]);
+		}
+	}
+	
+	function initVmcCommands(gcli) {
+		gcli.addCommand({
+			name: 'vmc',
+			description: 'Cloudfoundry Commandline Client',
+			manual: 'A nice manual for VMC goes in here'
+		});
+
+		gcli.addCommand({
+			name: 'vmc apps',
+			description: 'Reports apps installed on target',
+			manual: 'A nice manual for VMC goes in here',
+			params: [],
+			exec: execVmcApps
+		});
+		
+		gcli.addCommand({
+			name: 'vmc get-target',
+			description: 'Reports current target or sets a new target',
+			manual: 'A nice manual for VMC goes in here',
+			params: [],
+			exec: execVmcGetTarget
+		});
+		
+		gcli.addCommand({
+			name: 'vmc set-target',
+			description: 'Reports current target or sets a new target',
+			manual: 'A nice manual for VMC goes in here',
+			params: [
+					    {
+							name: 'target',
+							type: 'string',
+							description: 'Server target'
+					    }
+			],
+			exec: execVmcSetTarget
+		});
+		
+		gcli.addCommand({
+			name: 'vmc login',
+			description: 'Login to currenlty selected target',
+			manual: 'Login to currenlty selected target',
+			params: [
+					    {
+							name: 'email',
+							type: 'string',
+							description: "User's email address"
+					    },
+					    {
+							name: 'passwd',
+							type: 'string',
+							description: 'Password'
+						}
+			],
+			exec: execVmcLogin
+		});
+		
+		gcli.addCommand({
+			name: 'vmc push',
+			description: 'Deploy app to cloudfoundry',
+			manual: 'Deploy app to cloudfoundry',
+			params: [
+					    {
+							name: 'appname',
+							type: 'string',
+							description: "Appname"
+					    },
+					    {
+							name: 'url',
+							type: 'string',
+							description: 'Deployment URL',
+							defaultValue: null
+						},
+						{
+							name: 'instances',
+							type: 'number',
+							description: 'number of instances',
+							defaultValue: 1
+						},
+						{
+							name: 'mem',
+							type: 'number',
+							description: 'Memory (Mb)',
+							defaultValue: 512
+						},
+						{	
+							name: 'no-start',
+							type: 'boolean',
+							description: 'Do NOT start the app'
+						}
+			],
+			exec: execVmcPush
+		});
+		
+		simpleVMCCommands(gcli, ["start", "stop", "delete", "restart"]);
+	}
+	
+//	function initRooCommands() {
+//		gcli.addCommand({
+//			name: 'roo',
+//			description: 'Spring Roo Commands',
+//			manual: 'A nice manual for Roo goes here' 
+//		});
+//		
+//		gcli.addCommand({
+//			name: 'roo script',
+//			description: 'Execute a roo script',
+//			manual: 'A nice manual for the Roo script command goes in here',
+//			params: [
+//				{
+//					name: 'script',
+//					type: 'string',
+//					description: 'script name'
+//				}
+//			],
+//			exec: defaultCommandExec('/shellapi/roo/script')
+//		});
+//	}
+//	
+	function simpleMvnCommand(gcli, name) {
+		gcli.addCommand({
+			name: 'mvn '+name,
+			description: name+' project at current directory',
+			manual: 'A nice manual for mvn ' + name +' goes in here',
+			params: [
+			],
+			exec: defaultCommandExec('mvn/'+name)
+		});
+	}
+	
+	function simpleMvnCommands(gcli, listOfNames) {
+		for (var i = 0; i < listOfNames.length; i++) {
+			simpleMvnCommand(gcli, listOfNames[i]);			
+		}	
+	}
+	
+	function initMvnCommands(gcli) {
+		gcli.addCommand({
+			name: 'mvn',
+			description: 'Maven commands',
+			manual: 'A nice manual for Maven goes here' 
+		});
+		
+		simpleMvnCommands(gcli, [ 'assemble', 'build', 'compile', 'package', 'test' ]);
+	}
+		
+	////////////////////////////////////////////////////////////////////////
+	
+	function init(gcli) {
+		initVmcCommands(gcli);
+		initMvnCommands(gcli);
+		initNpmCommands(gcli);
+	}	
+
+	return {init: init};
+});
